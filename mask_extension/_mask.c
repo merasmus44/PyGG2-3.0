2f7dd41ffae13c3ca03c2a4b1ac8d286622aeacb
merasmus44 <azeruez@gmail.com>
2023-03-06 16:10:42 -0500

    added a todo

--- mask_extension/_mask.c
+++ mask_extension/_mask.c
@@ -19,7 +19,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
-
+//TODO: replace pyobjrct with pycapsules
 #include "_mask.h"
 #include "structmember.h"
 #include "bitmask.h"
