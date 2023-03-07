8bdaf968756d75a59311d3651743def31b6ec152
merasmus44 <azeruez@gmail.com>
2023-03-07 12:31:25 -0500

    Changed test

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 #a series of imports and other stuff that will fail if they don't work
-import mask_extension._mask
+from colorama import Fore
+try:
+    import mask_extension._mask
+except Exception as e:
+    print(Fore.RED + "Failed to import mask extension: \n" + str(e) + Fore.RESET)
+else:
+    print(Fore.GREEN + "Sucessfully imported the mask extension" + Fore.RESET)
 
